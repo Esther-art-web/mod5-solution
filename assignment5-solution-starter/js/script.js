9e9f6b7ae08aac04d1df1868d262fe318486d963
Ibeh Esther <estheribeh5@gmail.com>
2020-09-25 13:02:59 +0100

    modified the script.js file

--- assignment5-solution-starter/js/script.js
+++ assignment5-solution-starter/js/script.js
@@ -82,12 +82,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 
-var response= $ajaxUtils.sendGetRequest(
+$ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowCategoriesHTML
+       buildAndShowHomeHTML
+    , true
    // ***** <---- TODO: STEP 1: Substitute [...] ******
   );
-  console.log(response); // Explicitly setting the flag to get JSON from server processed into an object literal
+   // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
@@ -104,10 +105,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      
-      var chosenCategoryShortName =chooseRandomCategory(categories).short_name;
-
-
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -123,8 +121,6 @@ function buildAndShowHomeHTML (categories) {
       var html=homeHtml;
       
       var homeHtmlToInsertIntoMainPage = insertProperty(html,'randomCategoryShortName',chosenCategoryShortName)
-      
-
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
@@ -147,13 +143,13 @@ function chooseRandomCategory (categories) {
 
 
 // Load the menu categories view
-response=dc.loadMenuCategories = function () {
+dc.loadMenuCategories = function () {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
     allCategoriesUrl,
     buildAndShowCategoriesHTML);
 };
-console.log(response);
+
 
 
 // Load the menu items view
